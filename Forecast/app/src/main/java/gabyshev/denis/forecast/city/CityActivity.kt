c61548bc9daa8be600babbe5641850e519150189
dns21395 <dns21395@gmail.com>
2017-07-22 19:30:43 +0900

    city activity update

--- Forecast/app/src/main/java/gabyshev/denis/forecast/city/CityActivity.kt
+++ Forecast/app/src/main/java/gabyshev/denis/forecast/city/CityActivity.kt
@@ -1,8 +1,12 @@
 package gabyshev.denis.forecast.city
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import com.readystatesoftware.systembartint.SystemBarTintManager
 import gabyshev.denis.forecast.R
 
 /**
@@ -15,13 +19,24 @@ class CityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city)
-
-        Log.d(TAG, "onCreate")
-        getCityList()
+        transparentStatusBar()
 
     }
 
     private fun getCityList() {
         CityUtils.instance()?.getCityList(this);
+    }
+
+    private fun transparentStatusBar() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+        val tintManager = SystemBarTintManager(this)
+        // enable status bar tint
+        tintManager.isStatusBarTintEnabled = true
+        // enable navigation bar tint
+        tintManager.setNavigationBarTintEnabled(true)
     }
 }
