58afcfe2fd9552ff0be06c4b1fa15589a6534d15
dns21395 <dns21395@gmail.com>
2022-08-27 15:44:38 +0900

    обои для select_city

--- Forecast/app/src/main/java/gabyshev/denis/forecast/MainActivity.kt
+++ Forecast/app/src/main/java/gabyshev/denis/forecast/MainActivity.kt
@@ -3,13 +3,19 @@ package gabyshev.denis.forecast
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import gabyshev.denis.forecast.core.data.di.DaggerDataComponent
 import gabyshev.denis.forecast.core.di.ApplicationApi
+import gabyshev.denis.forecast.feature.select_city.SearchCity
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            SearchCity()
+        }
 
         val coreProvider = (applicationContext as ApplicationApi).coreProvider()
         val cityApi = DaggerDataComponent.factory().create(coreProvider).cityApi()
--- Forecast/feature/select_city/src/main/java/gabyshev/denis/forecast/feature/select_city/SearchCity.kt
+++ Forecast/feature/select_city/src/main/java/gabyshev/denis/forecast/feature/select_city/SearchCity.kt
@@ -1,8 +1,21 @@
 package gabyshev.denis.forecast.feature.select_city
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun SearchCity() {
-
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.city_background),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = null
+        )
+    }
 }
