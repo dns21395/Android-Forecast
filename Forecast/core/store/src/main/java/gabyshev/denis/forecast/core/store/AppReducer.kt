a9e370f4cace4f2e626de1d4d39a595c79ce8c0f
dns21395 <dns21395@gmail.com>
2022-08-27 15:57:52 +0900

    добавил core:store

---
+++ Forecast/core/store/src/main/java/gabyshev/denis/forecast/core/store/AppReducer.kt
@@ -0,0 +1,7 @@
+package gabyshev.denis.forecast.core.store
+
+import gabyshev.denis.forecast.core.redux.Action
+
+fun AppState.reduce(action: Action): AppState {
+    return this
+}
---
+++ Forecast/core/store/src/main/java/gabyshev/denis/forecast/core/store/AppStore.kt
@@ -0,0 +1,5 @@
+package gabyshev.denis.forecast.core.store
+
+import gabyshev.denis.forecast.core.redux.Store
+
+typealias AppStore = Store<AppState>
