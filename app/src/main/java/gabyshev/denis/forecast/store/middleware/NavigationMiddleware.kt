54322602fdc04daeb9db790f52672d442f2ff2ee
dns21395 <dns21395@gmail.com>
2022-12-13 21:54:35 +0300

    11: Контейнер для weather

--- app/src/main/java/gabyshev/denis/forecast/store/middleware/NavigationMiddleware.kt
+++ app/src/main/java/gabyshev/denis/forecast/store/middleware/NavigationMiddleware.kt
@@ -1,21 +1,21 @@
 package gabyshev.denis.forecast.store.middleware
 
 import com.github.terrakok.modo.stack.newStack
-import gabyshev.denis.forecast.Weather
 import gabyshev.denis.forecast.core.common.Navigation
 import gabyshev.denis.forecast.core.common.store.OpenSelectCityScreen
 import gabyshev.denis.forecast.core.redux.Action
 import gabyshev.denis.forecast.core.redux.Middleware
 import gabyshev.denis.forecast.core.store.AppState
+import gabyshev.denis.forecast.feature.weather.WeatherStack
 import javax.inject.Inject
 
 class NavigationMiddleware @Inject constructor(
-    private val navigation: Navigation
+    private val navigation: Navigation,
 ) : Middleware<AppState> {
 
     override fun bind(action: Action, state: AppState, dispatch: (Action) -> Unit) {
         when (action) {
-            is OpenSelectCityScreen -> navigation.getNavigation().newStack(Weather())
+            is OpenSelectCityScreen -> navigation.getNavigation().newStack(WeatherStack())
             else -> Unit
         }
     }
