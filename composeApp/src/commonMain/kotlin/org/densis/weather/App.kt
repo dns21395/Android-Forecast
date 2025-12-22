bec37efe2da1b26fc693eaf6087be28f0207b5cf
dns21395 <dns21395@gmail.com>
2025-12-22 20:53:53 +0500

    select city screen

--- composeApp/src/commonMain/kotlin/org/densis/weather/App.kt
+++ composeApp/src/commonMain/kotlin/org/densis/weather/App.kt
@@ -1,5 +1,7 @@
 package org.densis.weather
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -7,13 +9,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.densis.weather.select_city.SelectCity
 import org.densis.weather.select_city.SelectCityViewModel
 import org.densis.weather.select_city.presentation.SelectCityEffect
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
+import weather.composeapp.generated.resources.Res
+import weather.composeapp.generated.resources.city_background
 
 @Composable
 @Preview
@@ -31,9 +37,20 @@ fun App() {
         }
     }
 
-    SelectCity(
-        modifier = Modifier.fillMaxSize().statusBarsPadding().padding(horizontal = 16.dp),
-        state = state,
-        onEvent = { viewModel.onEvent(it) }
-    )
+    val painter = painterResource(Res.drawable.city_background)
+
+    Box(Modifier.fillMaxSize()) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+
+        SelectCity(
+            modifier = Modifier.fillMaxSize().statusBarsPadding().padding(horizontal = 16.dp),
+            state = state,
+            onEvent = { viewModel.onEvent(it) }
+        )
+    }
 }
--- composeApp/src/commonMain/kotlin/org/densis/weather/select_city/SelectCity.kt
+++ composeApp/src/commonMain/kotlin/org/densis/weather/select_city/SelectCity.kt
@@ -1,16 +1,25 @@
 package org.densis.weather.select_city
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.densis.weather.select_city.presentation.SelectCityEvent
 import org.densis.weather.select_city.presentation.SelectCityState
+import org.jetbrains.compose.resources.painterResource
+import weather.composeapp.generated.resources.Res
+import weather.composeapp.generated.resources.magnify
 
 @Composable
 fun SelectCity(
@@ -19,20 +28,33 @@ fun SelectCity(
     onEvent: (SelectCityEvent) -> Unit,
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
     ) {
         TextField(
             value = state.text,
-            onValueChange = { onEvent(SelectCityEvent.OnInputText(it)) }
+            onValueChange = { onEvent(SelectCityEvent.OnInputText(it)) },
+            placeholder = {
+                Text(
+                    "Enter a city name",
+                    style = MaterialTheme.typography.titleLarge
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(Res.drawable.magnify),
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = MaterialTheme.typography.titleLarge
         )
-        Button(
-            onClick = { onEvent(SelectCityEvent.OnShowToast) }
-        ) {
-            Text("click")
-        }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = state.yourText
-        )
+        Button(
+            onClick = { onEvent(SelectCityEvent.OnShowToast) },
+            modifier = Modifier.align(Alignment.End).padding(8.dp)
+        ) {
+            Text("search", style = MaterialTheme.typography.titleLarge)
+        }
     }
 }
